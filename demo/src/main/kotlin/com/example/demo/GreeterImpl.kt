ca59b6e6f5b632b72160f954e6a4c4141c1c4e4d
DESKTOP-4V10F99\matsumoto keisuke <keisuke.suke.san@gmail.com>
2022-01-24 17:41:56 +0900

    DI を実装しました

---
+++ demo/src/main/kotlin/com/example/demo/GreeterImpl.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.stereotype.Component
+
+interface Greeter {
+    fun sayHello(name: String): String
+}
+@Component
+class GreeterImpl: Greeter {
+    override fun sayHello(name: String) = "Hello $name"
+}
