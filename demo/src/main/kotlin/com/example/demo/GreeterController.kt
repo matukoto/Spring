6bad533fbe22945160e6e9b741a08df2a8eca7fc
DESKTOP-4V10F99\matsumoto keisuke <keisuke.suke.san@gmail.com>
2022-01-27 11:53:43 +0900

    フィールドインジェクションに関する記述を追加しました

--- demo/src/main/kotlin/com/example/demo/GreeterController.kt
+++ demo/src/main/kotlin/com/example/demo/GreeterController.kt
@@ -1,10 +1,14 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
-// 戻り値のオブジェクトを Json にシリアライズして返す
+// DI の方法その 1 コンストラクタインジェクション
 @RestController
 @RequestMapping("greeter")
+/* クラスの引数に Greeter 型の Greeter クラスを指定
+   これで Greeter クラスを実装しているクラス
+*/
 class GreeterController(private val greeter: Greeter) {
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
@@ -15,10 +19,18 @@ class GreeterController(private val greeter: Greeter) {
     fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
         return HelloResponse("Hello ${request.name}")
     }
-    //
+    /*
+        interface Greeter クラスを実装した GreeterImpl クラス
+        に実装されているメソッド（sayHello)を自動で生成してくれたんだね、ありがとう
+     */
     @GetMapping("/hello/byservice/{name}")
     fun helloByService (@PathVariable("name")name: String): HelloResponse {
         val message = greeter.sayHello(name)
         return HelloResponse(message)
     }
+   // フィールドインジェクション
+   // クラスのフィールドとして書く
+   // @Autowired
+   // private lateinit var greeter: Greeter
+
 }
--- demo/src/main/kotlin/com/example/demo/GreeterImpl.kt
+++ demo/src/main/kotlin/com/example/demo/GreeterImpl.kt
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component
 interface Greeter {
     fun sayHello(name: String): String
 }
+/*
+   Greeter クラス(interface)を実装
+   かつ
+   @Component アノテーションが付いたクラス
+   がインジェクションされる
+ */
 @Component
 class GreeterImpl: Greeter {
     override fun sayHello(name: String) = "Hello $name"
